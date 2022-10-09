f367cc493e586cc1e228e692aa32868d22f7f3b5
famrARM <fredy.moyano.mt@usa.edu.co>
2022-10-09 16:01:24 -0500

    reto4

---
+++ src/main/java/com/example/demo/Ret4G40Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ret4G40Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ret4G40Application.class, args);
+	}
+
+}
